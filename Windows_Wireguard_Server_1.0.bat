3c3421d978e8e487c709b92ac315b44906c62b34
pogoarte <98662243+pogoarte@users.noreply.github.com>
2025-02-08 22:52:35 +0100

    Update Windows_Wireguard_Server_1.0.bat

--- Windows_Wireguard_Server_1.0.bat
+++ Windows_Wireguard_Server_1.0.bat
@@ -62,54 +62,54 @@ curl https://download.wireguard.com/windows-client/wireguard-amd64-0.5.3.msi -o 
 curl http://pogoarte.altervista.org/files/apps/QREncode.exe -o QREncode.exe
 MsiExec /i WireGuard-amd64-0.5.3.msi /qn 
 taskkill /IM WireGuard /F 
-set PATH=%PATH%;C:\Programmi\WireGuard\ 
-mkdir C:\Programmi\WireGuard\Key 
-mkdir C:\Programmi\WireGuard\Config 
-move QREncode.exe C:\Programmi\WireGuard 
+set PATH=%PATH%;C:\Program Files\WireGuard\ 
+mkdir C:\Program Files\WireGuard\Key 
+mkdir C:\Program Files\WireGuard\Config 
+move QREncode.exe C:\Program Files\WireGuard 
 del WireGuard-amd64-0.5.3.msi
 
 cls 
-C:\Programmi\WireGuard\wg genkey > C:\Programmi\WireGuard\Key\wg_server.key 
-C:\Programmi\WireGuard\wg pubkey < C:\Programmi\WireGuard\Key\wg_server.key > C:\Programmi\WireGuard\Key\wg_server.pub 
-C:\Programmi\WireGuard\wg genkey > C:\Programmi\WireGuard\Key\wg_client.key 
-C:\Programmi\WireGuard\wg pubkey < C:\Programmi\WireGuard\Key\wg_client.key > C:\Programmi\WireGuard\Key\wg_client.pub 
-C:\Programmi\WireGuard\wg genpsk > C:\Programmi\WireGuard\Key\wg_client.psk 
+C:\Program Files\WireGuard\wg genkey > C:\Program Files\WireGuard\Key\wg_server.key 
+C:\Program Files\WireGuard\wg pubkey < C:\Program Files\WireGuard\Key\wg_server.key > C:\Program Files\WireGuard\Key\wg_server.pub 
+C:\Program Files\WireGuard\wg genkey > C:\Program Files\WireGuard\Key\wg_client.key 
+C:\Program Files\WireGuard\wg pubkey < C:\Program Files\WireGuard\Key\wg_client.key > C:\Program Files\WireGuard\Key\wg_client.pub 
+C:\Program Files\WireGuard\wg genpsk > C:\Program Files\WireGuard\Key\wg_client.psk 
 
 cls 
 set /P WG_SERVER_IP_DNS=WG SERVER IP or DNS (public not private): 
 set /P WG_SERVER_UDP_PORT=WG SERVER UDP PORT (forward on router): 
-set /P WG_SERVER.KEY=<C:\Programmi\WireGuard\Key\wg_server.key 
-set /P WG_SERVER.PUB=<C:\Programmi\WireGuard\Key\wg_server.pub 
-set /P WG_CLIENT.KEY=<C:\Programmi\WireGuard\Key\wg_client.key 
-set /P WG_CLIENT.PUB=<C:\Programmi\WireGuard\Key\wg_client.pub 
-set /P WG_CLIENT.PSK=<C:\Programmi\WireGuard\Key\wg_client.psk 
+set /P WG_SERVER.KEY=<C:\Program Files\WireGuard\Key\wg_server.key 
+set /P WG_SERVER.PUB=<C:\Program Files\WireGuard\Key\wg_server.pub 
+set /P WG_CLIENT.KEY=<C:\Program Files\WireGuard\Key\wg_client.key 
+set /P WG_CLIENT.PUB=<C:\Program Files\WireGuard\Key\wg_client.pub 
+set /P WG_CLIENT.PSK=<C:\Program Files\WireGuard\Key\wg_client.psk 
 
 cls 
-type nul > C:\Programmi\WireGuard\Config\wg_server.conf 
-echo [Interface] > C:\Programmi\WireGuard\Config\wg_server.conf 
-echo PrivateKey = %WG_SERVER.KEY% >> C:\Programmi\WireGuard\Config\wg_server.conf 
-echo ListenPort = %WG_SERVER_UDP_PORT% >> C:\Programmi\WireGuard\Config\wg_server.conf 
-echo MTU = 1420 >>  C:\Programmi\WireGuard\Config\wg_server.conf 
-echo Address = 10.0.69.1/24 >>  C:\Programmi\WireGuard\Config\wg_server.conf 
-echo [Peer] >>  C:\Programmi\WireGuard\Config\wg_server.conf 
-echo PublicKey = %WG_CLIENT.PUB% >>  C:\Programmi\WireGuard\Config\wg_server.conf 
-echo PresharedKey = %WG_CLIENT.PSK% >>  C:\Programmi\WireGuard\Config\wg_server.conf 
-echo AllowedIPs = 10.0.69.2/32 >>  C:\Programmi\WireGuard\Config\wg_server.conf 
+type nul > C:\Program Files\WireGuard\Config\wg_server.conf 
+echo [Interface] > C:\Program Files\WireGuard\Config\wg_server.conf 
+echo PrivateKey = %WG_SERVER.KEY% >> C:\Program Files\WireGuard\Config\wg_server.conf 
+echo ListenPort = %WG_SERVER_UDP_PORT% >> C:\Program Files\WireGuard\Config\wg_server.conf 
+echo MTU = 1420 >>  C:\Program Files\WireGuard\Config\wg_server.conf 
+echo Address = 10.0.69.1/24 >>  C:\Program Files\WireGuard\Config\wg_server.conf 
+echo [Peer] >>  C:\Program Files\WireGuard\Config\wg_server.conf 
+echo PublicKey = %WG_CLIENT.PUB% >>  C:\Program Files\WireGuard\Config\wg_server.conf 
+echo PresharedKey = %WG_CLIENT.PSK% >>  C:\Program Files\WireGuard\Config\wg_server.conf 
+echo AllowedIPs = 10.0.69.2/32 >>  C:\Program Files\WireGuard\Config\wg_server.conf 
 
 cls 
-type nul > C:\Programmi\WireGuard\Config\wg_client.conf 
-echo [Interface] > C:\Programmi\WireGuard\Config\wg_client.conf 
-echo PrivateKey = %WG_CLIENT.KEY% >> C:\Programmi\WireGuard\Config\wg_client.conf 
-echo Address = 10.0.69.2/24 >> C:\Programmi\WireGuard\Config\wg_client.conf 
-echo [Peer] >> C:\Programmi\WireGuard\Config\wg_client.conf 
-echo PublicKey = %WG_SERVER.PUB% >> C:\Programmi\WireGuard\Config\wg_client.conf 
-echo PresharedKey = %WG_CLIENT.PSK% >> C:\Programmi\WireGuard\Config\wg_client.conf 
-echo Endpoint = %WG_SERVER_IP_DNS%:%WG_SERVER_UDP_PORT% >> C:\Programmi\WireGuard\Config\wg_client.conf 
-echo AllowedIPs = 0.0.0.0/1,128.0.0.0/1 >> C:\Programmi\WireGuard\Config\wg_client.conf 
-echo PersistentKeepalive = 25 >> C:\Programmi\WireGuard\Config\wg_client.conf 
+type nul > C:\Program Files\WireGuard\Config\wg_client.conf 
+echo [Interface] > C:\Program Files\WireGuard\Config\wg_client.conf 
+echo PrivateKey = %WG_CLIENT.KEY% >> C:\Program Files\WireGuard\Config\wg_client.conf 
+echo Address = 10.0.69.2/24 >> C:\Program Files\WireGuard\Config\wg_client.conf 
+echo [Peer] >> C:\Program Files\WireGuard\Config\wg_client.conf 
+echo PublicKey = %WG_SERVER.PUB% >> C:\Program Files\WireGuard\Config\wg_client.conf 
+echo PresharedKey = %WG_CLIENT.PSK% >> C:\Program Files\WireGuard\Config\wg_client.conf 
+echo Endpoint = %WG_SERVER_IP_DNS%:%WG_SERVER_UDP_PORT% >> C:\Program Files\WireGuard\Config\wg_client.conf 
+echo AllowedIPs = 0.0.0.0/1,128.0.0.0/1 >> C:\Program Files\WireGuard\Config\wg_client.conf 
+echo PersistentKeepalive = 25 >> C:\Program Files\WireGuard\Config\wg_client.conf 
 
 cls 
-C:\Programmi\WireGuard\WireGuard /installtunnelservice "C:\Programmi\WireGuard\Config\wg_server.conf" 
+C:\Program Files\WireGuard\WireGuard /installtunnelservice "C:\Program Files\WireGuard\Config\wg_server.conf" 
 
 cls 
 powershell -command "Start-Sleep -s 3" 
@@ -118,7 +118,7 @@ powershell -command "New-NetNat -Name wg_server_nat -InternalIPInterfaceAddressP
 powershell -command "Set-NetConnectionProfile -InterfaceAlias 'wg_server' -NetworkCategory 'Private'" 
 
 cls 
-C:\Programmi\WireGuard\QREncode -r C:\Programmi\WireGuard\Config\wg_client.conf -o C:\Programmi\WireGuard\Config\wg_client.png -s 6 
-rundll32 "C:\Program Files\Windows Photo Viewer\photoviewer.dll",ImageView_Fullscreen C:\Programmi\WireGuard\Config\wg_client.png
+C:\Program Files\WireGuard\QREncode -r C:\Program Files\WireGuard\Config\wg_client.conf -o C:\Program Files\WireGuard\Config\wg_client.png -s 6 
+rundll32 "C:\Program Files\Windows Photo Viewer\photoviewer.dll",ImageView_Fullscreen C:\Program Files\WireGuard\Config\wg_client.png
 
 EXIT 0 
